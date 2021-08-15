80086a9e1f49e2a8afc45d1ce90a4cb473c27864
Ragan Martinez <ragan.martinez@live.com>
2021-08-15 18:40:24 +1000

    create layout for service layer and add article domain model

--- internal/domain/article.go
+++ internal/domain/article.go
@@ -1,1 +1,10 @@
 package domain
+
+// Article represents a news article
+type Article struct {
+	ID    string
+	Title string
+	Date  string
+	Body  string
+	Tags  []string
+}
--- internal/service/article.go
+++ internal/service/article.go
@@ -1,1 +1,40 @@
 package service
+
+import "github.com/rmar8138/article-rest-api/internal/domain"
+
+// ArticleRepository defines implementation of a repo to be passed into the service layer
+type ArticleRepository interface {
+	Get(id string) (domain.Article, error)
+	Create(CreateArticleInput) error
+}
+
+// ArticleService represents the service layer of articles
+type ArticleService struct {
+	repo ArticleRepository
+}
+
+// NewArticleService instantiates a new article service
+func NewArticleService(repo ArticleRepository) *ArticleService {
+	return &ArticleService{
+		repo: repo,
+	}
+}
+
+// CreateArticleInput is the shape of input needed for creating an article
+type CreateArticleInput struct {
+	ID    string
+	Title string
+	Date  string
+	Body  string
+	Tags  []string
+}
+
+// Get retrieves a single article by its id
+func (as *ArticleService) Get(id string) (domain.Article, error) {
+	return domain.Article{}, nil
+}
+
+// Create creates a new article
+func (as *ArticleService) Create(newArticle CreateArticleInput) error {
+	return nil
+}
