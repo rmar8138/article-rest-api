511fcfb5b5a49a8f96ed452ddacd9c738b4c905f
Ragan Martinez <ragan.martinez@live.com>
2021-08-15 18:30:31 +1000

    implement graceful server shutdown and basic logging

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -14,6 +18,12 @@ const (
 )
 
 func main() {
+	ctx := context.Background()
+
+	// setup log flags
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// setup router and middlewares
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
@@ -21,6 +31,28 @@ func main() {
 	articleHandler := rest.NewArticleHandler()
 	articleHandler.RegisterRoutes(r)
 
-	fmt.Printf("running server on port %v", PORT)
-	http.ListenAndServe(":"+PORT, r)
+	// setup graceful shutdown
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	srv := &http.Server{
+		Addr:    "localhost:" + PORT,
+		Handler: r,
+	}
+
+	log.Printf("running server on port %v", PORT)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("server failed to start: %v", err)
+		}
+	}()
+
+	<-stopChan
+	log.Print("shutting down server")
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("failed to shutdown gracefully: %v", err)
+	}
+
+	log.Print("server gracefully shutdown")
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/go-chi/chi v1.5.4
 	github.com/go-chi/render v1.0.1
+	github.com/sirupsen/logrus v1.8.1
 )
