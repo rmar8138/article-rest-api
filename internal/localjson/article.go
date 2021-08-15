53bce6a8c1df1dc87caf70718f538ffcf2fb4f3a
Ragan Martinez <ragan.martinez@live.com>
2021-08-15 18:46:26 +1000

    create basic shape of local json data store implementation

---
+++ internal/localjson/article.go
@@ -0,0 +1,35 @@
+package localjson
+
+import (
+	"github.com/rmar8138/article-rest-api/internal/domain"
+	"github.com/rmar8138/article-rest-api/internal/service"
+)
+
+const (
+	// FILEPATH is the filepath to the fake articles json file relative to root of repo
+	FILEPATH = "internal/localjson/data/articles.json"
+)
+
+// ArticleRepository represents a local json file implementation of the data
+// persistence layer
+type ArticleRepository struct {
+	articlesFilepath string
+}
+
+// NewArticleRepository instantiates a new implementation of a local json memory store for articles
+func NewArticleRepository(articlesFilepath string) *ArticleRepository {
+	return &ArticleRepository{
+		articlesFilepath: articlesFilepath,
+	}
+}
+
+// Get retrieves an article from a json file store based on a string id
+func (ar *ArticleRepository) Get(id string) (domain.Article, error) {
+	return domain.Article{}, nil
+}
+
+// Create creates a new article in a json file store by appending to the existing file if
+// article with given ID does not yet exist
+func (ar *ArticleRepository) Create(newArticle service.CreateArticleInput) error {
+	return nil
+}
